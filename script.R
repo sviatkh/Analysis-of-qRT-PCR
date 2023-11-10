bdf017f7b6ebb33a224f0da6437573ba1775f94c
sviatkh <sviatoslavkharuk@gmail.com>
2023-11-10 22:15:04 +0200

    add grouping for sample names

--- script.R
+++ script.R
@@ -5,15 +5,34 @@ library(DescTools)
 library(ggplot2)
 
 # data wrangling 
-
+data <- read_excel("../Example_data.xlsx")
 # group by Target
 new_df <- data[order(data$Target), ]
 
 # sample the dataframe and create new column Group
-sample_group_ <- data.frame(
+
+sample_group_1 <- data.frame(
   sample_name = c("414", "415", "417", "418"),
   group_name = "Control"
 )
+
+sample_group_2 <- data.frame(
+  sample_name = c("428", "430", "431", "432"),
+  group_name = "CD"
+)
+
+sample_group_3 <- data.frame(
+  sample_name = c("483", "484", "485", "486"),
+  group_name = "AK"
+)
+
+sample_group_4 <- data.frame(
+  sample_name = c("423", "424", "426", "427"),
+  group_name = "CDA"
+)
+
+sample_group <- rbind(sample_group_1, sample_group_2, sample_group_3, sample_group_4)
+
 
 # new column with NA
 new_df$Group <- NA
@@ -25,11 +44,11 @@ for (i in 1:nrow(sample_group)) {
 
 
 # data reorder 
-data$Group <- factor(data$Group, levels = c("Control", "CD", "AKG", "CD + AKG"))
+new_df$Group <- factor(new_df$Group, levels = c("Control", "CD", "AK", "CDA"))
 
+unique(new_df$Target)
 
-
-ggplot(data, aes(Target, Ct)) +  
+ggplot(new_df, aes(Target, Ct)) +  
   geom_violin(position = position_dodge(width = .7), width = 2, aes(fill = Group), outlier.colour = NA, draw_quantiles = c(0.25, 0.5, 0.75)) + 
   #geom_jitter(position = position_jitter(width = 1), alpha = 0.5) + 
   labs(y = "Relative expression", x = " ") + 
